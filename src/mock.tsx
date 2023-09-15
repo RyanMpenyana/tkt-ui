67e40cd2f995a985e87b412c912fc1bbb2fef1b4
vmpenyana <vincent@tenxrs.com>
2023-09-15 08:39:45 +0200

    added mock events

---
+++ src/mock.tsx
@@ -0,0 +1,72 @@
+export const events = [
+    {
+        "id": 2,
+        "description": "Hard Rock Cafe, Going Hard!",
+        "tickets": [],
+        "venue": "Sandton",
+        "time": "14:00:00",
+        "date": "2023-10-14",
+        "province": "Gauteng",
+        "sales": []
+    },
+    {
+        "id": 4,
+        "description": "Hard Rock Cafe, Going Hard!",
+        "tickets": [],
+        "venue": "Sandton",
+        "time": "14:00:00",
+        "date": "2023-10-14",
+        "province": "Gauteng",
+        "sales": []
+    },
+    {
+        "id": 3,
+        "description": "Hard Rock Cafe, Going Hard!",
+        "tickets": [],
+        "venue": "Sandton",
+        "time": "14:00:00",
+        "date": "2023-10-14",
+        "province": "Gauteng",
+        "sales": []
+    },
+    {
+        "id": 6,
+        "description": "Hard Rock Cafe, Going Hard!",
+        "tickets": [],
+        "venue": "Sandton",
+        "time": "14:00:00",
+        "date": "2023-10-14",
+        "province": "Gauteng",
+        "sales": []
+    },
+    {
+        "id": 5,
+        "description": "Hard Rock Cafe, Going Hard!",
+        "tickets": [],
+        "venue": "Sandton",
+        "time": "14:00:00",
+        "date": "2023-10-14",
+        "province": "Gauteng",
+        "sales": []
+    },
+    {
+        "id": 7,
+        "description": "Dance dance!",
+        "tickets": [],
+        "venue": "Sandton",
+        "time": "14:00:00",
+        "date": "2023-10-14",
+        "province": "Gauteng",
+        "sales": []
+    },
+    {
+        "id": 1,
+        "description": "It's private picnic",
+        "tickets": [],
+        "venue": "Sandton",
+        "time": "14:00:00",
+        "date": "2023-10-14",
+        "province": "Gauteng",
+        "sales": []
+    }
+]
